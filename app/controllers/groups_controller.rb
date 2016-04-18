b25ec2e0197999ba5bfe9bcae6f220f1a6060a2f
Jason Lung <lionlies@gmail.com>
2016-04-18 21:50:46 +0800

    Added ability for an user to join and quit a group.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,7 @@ class GroupsController < ApplicationController
     @group = current_user.groups.create(group_params)
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -42,6 +43,32 @@ class GroupsController < ApplicationController
     @group = current_user.groups.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "討論版已刪除"  # 這邊可以加入“刪除確認”的功能。
+  end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "成功加入本討論區！"
+    else
+      flash[:warning] = "你之前就加入了！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "成功退出本討論區！"
+    else
+      flash[:warning] = "你本來就沒加入這個討論板！"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   resources :groups do
     resources :posts
+
+    member do
+      post :join
+      post :quit
+    end
+
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
