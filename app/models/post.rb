7d643689c41e8946b734dcef4268300dda584a21
Jason Lung <lionlies@gmail.com>
2016-04-20 19:24:25 +0800

    Refactored code.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+
+  scope :recent, -> {order("updated_at DESC")}
+
   belongs_to :group, counter_cache: :posts_count
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
@@ -7,4 +10,5 @@ class Post < ActiveRecord::Base
   def editable_by?(user)
     user && user == author
   end
+
 end
